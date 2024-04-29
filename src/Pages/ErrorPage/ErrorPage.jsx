d6980e10f16356c11f972e0801204fed969eb262
Maruf Hossain <mdmarufh974@gmail.com>
2024-04-30 02:46:40 +0600

    404 page img fixing

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
         <div>
             <Navber></Navber>
             <div className="max-w-7xl mx-auto flex flex-col items-center">
-                <img src="/src/assets/images/404.png" alt="" />
+                <img src="../../assets/images/404.png" alt="" />
                 <Link><button className="bg-gradient-to-r from-[#e39396] via-purple-500 to-pink-500 mb-20 px-10 rounded-full py-2 text-white font-medium">Back to Home</button></Link>
             </div>
             <Footer></Footer>
